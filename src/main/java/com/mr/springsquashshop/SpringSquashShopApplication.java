070f962d5471b36fe19c5684e0da43d9790fa430
MarcoRy <rydzewski.marek86@gmail.com>
2020-03-10 21:49:59 +0100

    Initialized squash-shop-project

---
+++ src/main/java/com/mr/springsquashshop/SpringSquashShopApplication.java
@@ -0,0 +1,13 @@
+package com.mr.springsquashshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSquashShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSquashShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mr/springsquashshop/SpringSquashShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mr.springsquashshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSquashShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
