a4e131f0be0bd531f49e0874b5b82824413f75dd
MarcoRy <rydzewski.marek86@gmail.com>
2020-03-14 09:10:01 +0100

    Created Product entity

---
+++ src/main/java/com/mr/springsquashshop/dao/Product.java
@@ -0,0 +1,172 @@
+package com.mr.springsquashshop.dao;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Entity
+@Table(name = "product")
+public class Product {
+
+    @Id
+    @GeneratedValue( strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "sku")
+    private String sku;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "description")
+    private String description;
+    @Column(name = "unit_price")
+    private BigDecimal unitPrice;
+    @Column(name = "image_url")
+    private String imageUrl;
+    @Column(name = "active")
+    private boolean active;
+    @Column(name = "units_in_stock")
+    private int unitsInStock;
+    @Column(name = "date_created")
+    @CreationTimestamp
+    private LocalDateTime dateCreated;
+    @Column(name = "last_updated")
+    @UpdateTimestamp
+    private LocalDateTime lastUpdated;
+
+
+    public Product(String sku, String name, String description, BigDecimal unitPrice, String imageUrl, boolean active, int unitsInStock) {
+        this.sku = sku;
+        this.name = name;
+        this.description = description;
+        this.unitPrice = unitPrice;
+        this.imageUrl = imageUrl;
+        this.active = active;
+        this.unitsInStock = unitsInStock;
+
+    }
+
+    public Product() {
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getSku() {
+        return sku;
+    }
+
+    public void setSku(String sku) {
+        this.sku = sku;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(BigDecimal unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public int getUnitsInStock() {
+        return unitsInStock;
+    }
+
+    public void setUnitsInStock(int unitsInStock) {
+        this.unitsInStock = unitsInStock;
+    }
+
+    public LocalDateTime getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(LocalDateTime dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    public LocalDateTime getLastUpdated() {
+        return lastUpdated;
+    }
+
+    public void setLastUpdated(LocalDateTime lastUpdated) {
+        this.lastUpdated = lastUpdated;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return active == product.active &&
+                unitsInStock == product.unitsInStock &&
+                Objects.equals(id, product.id) &&
+                Objects.equals(sku, product.sku) &&
+                Objects.equals(name, product.name) &&
+                Objects.equals(description, product.description) &&
+                Objects.equals(unitPrice, product.unitPrice) &&
+                Objects.equals(imageUrl, product.imageUrl) &&
+                Objects.equals(dateCreated, product.dateCreated) &&
+                Objects.equals(lastUpdated, product.lastUpdated);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, sku, name, description, unitPrice, imageUrl, active, unitsInStock, dateCreated, lastUpdated);
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", sku='" + sku + '\'' +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", unitPrice=" + unitPrice +
+                ", imageUrl='" + imageUrl + '\'' +
+                ", active=" + active +
+                ", unitsInStock=" + unitsInStock +
+                ", dateCreated=" + dateCreated +
+                ", lastUpdated=" + lastUpdated +
+                '}';
+    }
+}
