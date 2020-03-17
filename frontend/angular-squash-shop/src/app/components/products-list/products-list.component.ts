2b4eef2d2d17d1710c33fd8e3935bae677192e5a
MarcoRy <rydzewski.marek86@gmail.com>
2020-03-17 21:59:11 +0100

    Created method to get products list and put it to products variable

--- frontend/angular-squash-shop/src/app/components/products-list/products-list.component.ts
+++ frontend/angular-squash-shop/src/app/components/products-list/products-list.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ProductService} from '../../services/product.service';
+import {Product} from "../../common/product";
 
 @Component({
   selector: 'app-products-list',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsListComponent implements OnInit {
 
-  constructor() { }
+  products: Product[];
 
-  ngOnInit() {
+  constructor(private productService: ProductService) {
   }
 
+  ngOnInit() {
+    this.getProducts()
+  }
+
+  getProducts() {
+    this.productService.getProductsList().subscribe(data => this.products = data);
+  }
 }
