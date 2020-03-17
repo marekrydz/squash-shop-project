b49bd56f97f03a7c84b99a6a9e39d58071b4860a
MarcoRy <rydzewski.marek86@gmail.com>
2020-03-17 19:05:17 +0100

    Created getProductsList() to map JSON data to Product list. Declared  GetResponse interface to unwarps JSON data

--- frontend/angular-squash-shop/src/app/services/product.service.ts
+++ frontend/angular-squash-shop/src/app/services/product.service.ts
@@ -1,9 +1,26 @@
 import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from "rxjs";
+import {Product} from "../common/product";
+import {map} from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  private url: string = "api/products";
+
+  constructor(private http: HttpClient) {
+  }
+
+  getProductsList(): Observable<Product[]> {
+    return this.http.get<GetResponse>(this.url).pipe(map(response => response._embedded.products))
+  }
+}
+
+export interface GetResponse {
+  _embedded: {
+    products: Product [];
+  }
 }
