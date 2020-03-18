7c15565af8314b825fff26405af8f88fcf908abf
MarcoRy <rydzewski.marek86@gmail.com>
2020-03-18 22:20:16 +0100

    Added @CrossOrigin to make ability to get request from frontend layer and send response

--- backend/spring-squash-shop/src/main/java/com/mr/springsquashshop/repository/ProductCategoryRepository.java
+++ backend/spring-squash-shop/src/main/java/com/mr/springsquashshop/repository/ProductCategoryRepository.java
@@ -3,7 +3,9 @@ package com.mr.springsquashshop.repository;
 import com.mr.springsquashshop.entity.ProductCategory;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RepositoryRestResource(path = "categories", itemResourceRel = "categories")
 public interface ProductCategoryRepository extends CrudRepository<ProductCategory, Long> {
 
