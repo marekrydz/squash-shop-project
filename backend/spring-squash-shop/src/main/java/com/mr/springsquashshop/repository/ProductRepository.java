60ce734aa451e3c7f87ce512d3e84ea31febe7ac
MarcoRy <rydzewski.marek86@gmail.com>
2020-03-18 22:20:16 +0100

    Added @CrossOrigin to make ability to get request from frontend layer and send response

--- backend/spring-squash-shop/src/main/java/com/mr/springsquashshop/repository/ProductRepository.java
+++ backend/spring-squash-shop/src/main/java/com/mr/springsquashshop/repository/ProductRepository.java
@@ -3,7 +3,9 @@ package com.mr.springsquashshop.repository;
 import com.mr.springsquashshop.entity.Product;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RepositoryRestController
 public interface ProductRepository extends CrudRepository<Product,Long> {
 }
