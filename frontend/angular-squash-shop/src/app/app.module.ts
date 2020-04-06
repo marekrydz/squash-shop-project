8af012e8032e069981537304502918dd5e7c88e1
MarcoRy <rydzewski.marek86@gmail.com>
2020-04-06 19:41:21 +0200

    Implemented RouterModule and Routes to project. Created Routes array to handle categories.

--- frontend/angular-squash-shop/src/app/app.module.ts
+++ frontend/angular-squash-shop/src/app/app.module.ts
@@ -5,6 +5,15 @@ import {AppComponent} from './app.component';
 import {ProductsListComponent} from './components/products-list/products-list.component';
 import {HttpClientModule} from '@angular/common/http';
 import {ProductService} from './services/product.service';
+import {RouterModule, Routes} from '@angular/router';
+
+const appRoutes: Routes = [
+  {path: 'category/:id', component: ProductsListComponent},
+  {path: 'category', component: ProductsListComponent},
+  {path: 'products', component: ProductsListComponent},
+  {path: '',  redirectTo: '/products', pathMatch: 'full'},
+  {path: '**', redirectTo: '/products', pathMatch: 'full'}
+];
 
 @NgModule({
   declarations: [
@@ -13,8 +22,12 @@ import {ProductService} from './services/product.service';
   ],
   imports: [
     HttpClientModule,
-    BrowserModule
-  ],
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
+],
   providers: [ProductService],
   bootstrap: [AppComponent]
 })
