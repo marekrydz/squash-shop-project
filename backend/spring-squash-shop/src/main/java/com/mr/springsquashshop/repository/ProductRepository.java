fc61f9adbe199d55754f0160e49f39bade1e4f76
MarcoRy <rydzewski.marek86@gmail.com>
2020-04-09 20:22:16 +0200

    Added findByCategoryId to send product selected by specified category

--- backend/spring-squash-shop/src/main/java/com/mr/springsquashshop/repository/ProductRepository.java
+++ backend/spring-squash-shop/src/main/java/com/mr/springsquashshop/repository/ProductRepository.java
@@ -1,11 +1,17 @@
 package com.mr.springsquashshop.repository;
 
 import com.mr.springsquashshop.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @CrossOrigin(origins = "http://localhost:4200")
 @RepositoryRestController
 public interface ProductRepository extends CrudRepository<Product,Long> {
+
+    Page<Product> findByCategoryId (@RequestParam(name = "id") Long id, Pageable pageable);
+
 }
