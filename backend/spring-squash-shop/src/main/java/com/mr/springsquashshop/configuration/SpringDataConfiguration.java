4e9be0ab089a61d68a2d09af62870b1325de54b6
MarcoRy <rydzewski.marek86@gmail.com>
2020-04-15 20:35:27 +0200

    Expose id's for entity responses.

--- backend/spring-squash-shop/src/main/java/com/mr/springsquashshop/configuration/SpringDataConfiguration.java
+++ backend/spring-squash-shop/src/main/java/com/mr/springsquashshop/configuration/SpringDataConfiguration.java
@@ -2,14 +2,25 @@ package com.mr.springsquashshop.configuration;
 
 import com.mr.springsquashshop.entity.Product;
 import com.mr.springsquashshop.entity.ProductCategory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.core.mapping.ExposureConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
 
+import javax.persistence.EntityManager;
+import javax.persistence.metamodel.EntityType;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 @Configuration
 public class SpringDataConfiguration implements RepositoryRestConfigurer {
+
+    @Autowired
+    EntityManager entityManager;
+
 
     @Override
     public void configureRepositoryRestConfiguration(RepositoryRestConfiguration restConfig) {
@@ -20,6 +31,20 @@ public class SpringDataConfiguration implements RepositoryRestConfigurer {
 
         config.forDomainType(ProductCategory.class).withItemExposure((metadata, httpMethods) ->
                 httpMethods.disable(HttpMethod.PATCH, HttpMethod.PUT, HttpMethod.DELETE));
+
+        exposeId(restConfig);
     }
 
+    private void exposeId(RepositoryRestConfiguration restConfig) {
+
+        Set<EntityType<?>> entityTypes = entityManager.getMetamodel().getEntities();
+        List<Class> classList = new ArrayList<>();
+
+        for (EntityType entityType : entityTypes) {
+            classList.add(entityType.getJavaType());
+        }
+
+        Class[] domainObjects = classList.toArray(new Class[0]);
+        restConfig.exposeIdsFor(domainObjects);
+    }
 }
